9c488ad2a0e4137b54b359aeda388a92df9e16ba
Nico Baier <nico.baier@gmail.com>
2024-02-24 16:41:03 -0600

    Add @octokit/rest dependency and update code to use Octokit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,55 @@
-console.log("Hello via Bun!");
+import { Octokit } from "@octokit/rest";
+
+const client = new Octokit({
+  auth: Bun.env.GH_REPO_TOKEN,
+});
+
+const owner = "nbbaier";
+const repo = "test-ground";
+const commits = await client.rest.repos.listCommits({
+  owner,
+  repo,
+});
+
+const commitSHA = commits.data[0].sha;
+
+const {
+  data: { sha: currentTreeSHA },
+} = await client.rest.git.createTree({
+  owner,
+  repo,
+  tree: [
+    {
+      path: "third_new_file.txt",
+      content: "will this go onto branch_1",
+      mode: "100644",
+      type: "commit",
+    },
+  ],
+  base_tree: commitSHA,
+  message: "Updated programatically with Octokit",
+  parents: [commitSHA],
+});
+
+console.log("currentTreeSHA: ", currentTreeSHA);
+
+const {
+  data: { sha: newCommitSHA },
+} = await client.rest.git.createCommit({
+  owner,
+  repo,
+  tree: currentTreeSHA,
+  message: "Updated programatically with Octokit",
+  parents: [commitSHA],
+});
+
+console.log("newCommitSHA: ", newCommitSHA);
+
+console.log(
+  await client.rest.git.updateRef({
+    owner,
+    repo,
+    ref: "heads/main",
+    sha: newCommitSHA,
+  })
+);
